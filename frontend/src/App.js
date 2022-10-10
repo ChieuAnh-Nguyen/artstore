74f9184c8899672145dceee34f9a8a236e2e7e69
ChieuAnh-Nguyen <chieuanh.nguyen@sjsu.edu>
2022-10-10 14:44:21 -0700

    First commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 
+import React from "react"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
-      <h1>Welcome to Art Store</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Art Store</h1>
+        </Container>
+      </main>
+      <Footer />
+
     </>
   );
 }
