d6251d28be1516e5e23d66354103c786f10e3aeb
ChieuAnh-Nguyen <chieuanh.nguyen@sjsu.edu>
2022-10-14 17:56:07 -0700

    added rating system

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import {Row, Col} from "react-bootstrap"
-import products from "../products"
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
+      <h1>Latest Products</h1>
+      <Row>
         {products.map((product) => (
-            <Col sm = {12} md={6} lg={4} xl={3}>
-             <Product product={product}/>
-            </Col>
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
-    </Row>
+      </Row>
     </>
-  )
+  );
+};
 
-
-}
-
-export default HomeScreen
+export default HomeScreen;
