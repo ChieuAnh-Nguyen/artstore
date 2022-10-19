c707a2fe62bf4e23bad3cd2275d7dd804f1a70ef
ChieuAnh-Nguyen <chieuanh.nguyen@sjsu.edu>
2022-10-18 19:55:52 -0700

    add to cart and shopping cart features added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,28 +3,27 @@ import {
   combineReducers,
   applyMiddleware,
 } from "redux"
-// import { configureStore } from "@reduxjs/toolkit"
-
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import {
   productListReducer,
   productDetailsReducer,
-  productDeleteReducer,
-  productCreateReducer,
-  productUpdateReducer,
-  productReviewCreateReducer,
-  productTopRatedReducer,
 } from "./reducers/productReducers.js"
-
+import { cartReducer } from "./reducers/cartReducers"
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : []
 
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 const middleware = [thunk]
-
 const store = createStore(
   reducer,
   initialState,
